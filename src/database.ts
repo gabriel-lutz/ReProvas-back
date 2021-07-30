c5eeabf38ee0ff33114b829d7ca0ba6fef91dbe3
Gabriel Peres Lutz <gabriel.p.lutz@gmail.com>
2021-07-29 22:40:06 -0300

    real fix deploy test v2

--- src/database.ts
+++ src/database.ts
@@ -1,3 +1,4 @@
+ 
 import { getConnectionManager } from "typeorm";
 
 if (process.env.NODE_ENV === 'production' && process.env.DATABASE_URL.indexOf("sslmode=require") === -1) {
@@ -10,7 +11,7 @@ export default async function connect () {
     name: "default",
     type: "postgres",
     url: process.env.DATABASE_URL,
-    entities: [`${process.env.NODE_ENV === 'production' ? 'dist' : 'src'}/entities/.`],
+    entities: [`${process.env.NODE_ENV === 'production' ? 'dist' : 'src'}/entities/*.*`],
     ssl: process.env.NODE_ENV === 'production'
   });
   await connection.connect();
